3ea4927609bd4042a4c45507772c60cddae5a89e
Hamza Abouabid <annoncecbh@gmail.com>
2024-10-30 17:32:13 +0100

    add GI

---
+++ GI/G1/Serie1/ex2.py
@@ -0,0 +1,38 @@
+# ex2 série 1 Opérateurs arithmétiques
+
+# Déclaration de variables
+
+a = int(input("Entrez la valeur de a: "))
+b = int(input("Entrez la valeur de b: "))
+
+# Addition
+addition = a + b
+
+# Soustraction
+soustraction = a - b
+
+# Multiplication
+
+multiplication = a * b
+
+# Division
+division = a / b
+
+# Division entière
+division_entiere = a // b
+
+# Modulo
+modulo = a % b
+
+# Puissance
+puissance = a ** b
+
+# Affichage des résultats
+print(f""" opérations arithmétiques sur {a} et {b}:
+Addition: {addition}
+Soustraction: {soustraction}
+Multiplication: {multiplication}
+Division: {division}
+Division entière: {division_entiere}
+Modulo: {modulo}
+""")
---
+++ GI/G2/serie1/ex2.py
@@ -0,0 +1,28 @@
+# ex2 série 1 op arithmitique
+
+a = 7
+b = 8
+
+add = a + b
+
+sous = a - b
+
+mul = a * b
+
+div = a / b
+
+mod = a % b
+
+exp = a ** b
+
+floordiv = a // b
+
+
+
+print(f"La somme de {a} et {b} est {add}")
+print(f"La différence entre {a} et {b} est {sous}")
+print(f"Le produit de {a} et {b} est {mul}")
+print(f"Le quotient de {a} divisé par {b} est {div}")
+print(f"Le reste de la division de {a} par {b} est {mod}")
+print(f"{a} élevé à la puissance {b} est {exp}")
+print(f"Le quotient entier de {a} divisé par {b} est {floordiv}")
